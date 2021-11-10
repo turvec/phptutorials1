1244e5be3f3b8ea312b44b66f618b5371c6cf3f8
turvec <okaforturvec@gmail.com>
2021-11-10 12:00:19 -0800

    commit

--- add_action.php
+++ add_action.php
@@ -6,9 +6,9 @@ include_once "connection.php";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-echo "   ".$name;
-echo "   ".$email;
-echo "   ".$password;
+echo " ".$name;
+echo " ".$email;
+echo " ".$password;
 
 $sql="INSERT into users (name,email,password)
 VALUES('$name','$email','$password')";
